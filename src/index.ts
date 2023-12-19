1e8fecc2986cae5c01cfa9638d617f773398d68b
Andrey Tereschenko <atereschenko@1aauto.inc>
2023-12-19 15:47:58 +0300

    added azure artifact for npm publish

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ const [nodePath, cachePath, command, ...params] = process.argv;
 if (['build', 'deploy', 'run'].includes(command)) {
   const [project, profile, ...rest] = params;
 
+  if (!project) {
+    console.log('Project name is not defined!')
+    process.exit(1)
+  }
+
   console.log("command", command, "project", project, "profile", profile);
 } else {
   console.log("command", command, "params", params);
